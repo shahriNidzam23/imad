dfaa6a317e9dd3c9fc0d398c153a9c6c45092269
Shahrin Nidzam <shahrin.nidzam@gmail.com>
2019-08-16 14:01:22 +0800

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
+  { path: '', loadChildren: './home/home.module#HomePageModule' },
   { path: 'imad', loadChildren: './imad/imad.module#ImadPageModule' },
   { path: 'contact', loadChildren: './contact/contact.module#ContactPageModule' },
   { path: 'about', loadChildren: './about/about.module#AboutPageModule' },
--- src/app/iqra-info/iqra-info.page.ts
+++ src/app/iqra-info/iqra-info.page.ts
@@ -8,14 +8,18 @@ import {IonSlides} from '@ionic/angular';
   styleUrls: ['./iqra-info.page.scss'],
 })
 export class IqraInfoPage implements OnInit {
-  slides:Array<{img:string}>;
+  slides:Array<{img:string,sound:string, text:string}>;
   id:any;
+  text:any;
   @ViewChild('alph') slider: IonSlides;
   constructor(private route: ActivatedRoute) { 
     this.slides = [];
+    this.text = this.letter();
     for(let i = 0; i < 30; i++) {
       this.slides.push({
-        img:"assets/image/letter/" + (i + 1) + ".PNG"
+        img:"assets/image/letter/" + (i + 1) + ".PNG",
+        sound:"assets/sound/" + (i + 1) + ".m4a",
+        text: this.text[i]
       });
     }
   }
@@ -25,6 +29,41 @@ export class IqraInfoPage implements OnInit {
     console.log(this.id);
 
     this.slider.slideTo(parseInt(this.id) - 1, 10);
+  }
+
+  letter(){
+    return [
+      ["A", "LIF"],
+      ["BA"],
+      ["TA"],
+      ["TSA"],
+      ["JIM"],
+      ["KHA"],
+      ["KHO"],
+      ["DAL"],
+      ["DZAL"],
+      ["RO"],
+      ["ZAI"],
+      ["SIN"],
+      ["SYIN"],
+      ["SHAD"],
+      ["DHAD"],
+      ["THO"],
+      ["DZO"],
+      ["AIN"],
+      ["GHAIN"],
+      ["FA"],
+      ["QAF"],
+      ["KAF"],
+      ["LAM"],
+      ["MIM"],
+      ["NUN"],
+      ["WAU"],
+      ["HAA"],
+      ["LAM", "A", "LIF"],
+      ["HAM", "ZA"],
+      ["YAA"]
+    ];
   }
 
 }
